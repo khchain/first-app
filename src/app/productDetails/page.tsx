8c0760ca31aa9a0a85a2bc9bdbf46f8073f1e804
Hamid <142207147+khchain@users.noreply.github.com>
2023-11-25 12:16:29 +0330

    Revert "add search box and get data function"
    This reverts commit 2f8ca3f56bfe6f6d121f8154bf576d77a0848978.

--- src/app/productDetails/page.tsx
+++ src/app/productDetails/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { ProductDetails } from '@/components/productDetails'
 
-
 export default function ProductDetailsPage() {
-  
   return (
     <ProductDetails />
   )
